71b0cda650576e5723127032904e8aa8435921c8
thomaslealeikhaugen-ux <thomaslealeikhaugen@gmail.com>
2026-05-23 17:20:48 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Moderating and more! Made by Thomas_le123 ",
         // Activity type number (0 = Playing).
         type: 0, 
       },
