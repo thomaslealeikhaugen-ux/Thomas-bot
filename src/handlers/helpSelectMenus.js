752ec49ecc43f1405a36a71eb8b871ce989c8b4b
thomaslealeikhaugen-ux <thomaslealeikhaugen@gmail.com>
2026-05-23 17:21:46 +0200

    Update helpSelectMenus.js

--- src/handlers/helpSelectMenus.js
+++ src/handlers/helpSelectMenus.js
@@ -13,7 +13,7 @@ const BACK_BUTTON_ID = "help-back-to-main";
 const ALL_COMMANDS_ID = "help-all-commands";
 const PAGINATION_PREFIX = "help-page";
 const CATEGORY_SELECT_ID = "help-category-select";
-const FOOTER_TEXT = "Made with ❤️";
+const FOOTER_TEXT = "Moderating and more! Made by Thomas_le123 ";
 const SUBCOMMAND_TYPE = 1;
 const SUBCOMMAND_GROUP_TYPE = 2;
 
